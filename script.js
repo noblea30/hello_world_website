a4c247ec9b5f98541a9ce8ba861b1159cc7a11be
noblea30 <91907956+noblea30@users.noreply.github.com>
2023-03-20 20:19:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello, World!");
+const uploadBtn = document.getElementById("upload-btn");
+
+uploadBtn.addEventListener("click", function() {
+  const fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.addEventListener("change", function() {
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+    reader.addEventListener("load", function() {
+      const fileContent = reader.result;
+      const displayArea = document.createElement("pre");
+      displayArea.textContent = fileContent;
+      document.body.appendChild(displayArea);
+    });
+    reader.readAsText(file);
+  });
+  fileInput.click();
+});
+
+
+
+
